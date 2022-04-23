76bd59f4a36126a5832985e3211d38ec38ab785e
Marc Brakken <marc.brakken@gmail.com>
2022-04-23 11:50:25 -0500

    start redis client correctly

--- utilities/redisClient.js
+++ utilities/redisClient.js
@@ -1,15 +1,8 @@
 const { createClient } = require('redis');
 
-console.log('ENV', process.env);
+const redisURL = process.env.REDIS_TLS_URL ||  process.env.REDIS_URL || 'redis://localhost:6379';
 
-const redisURL =
-  process.env.REDIS_TLS_URL ||
-  process.env.REDIS_URL ||
-  process.env.REDISTOGO_URL;
-
-console.log('redisURL', redisURL);
-
-const client = createClient(redisURL);
+const client = createClient({ url: redisURL });
 
 client.on('error', (err) => console.log('Redis Client Error', err));
 
