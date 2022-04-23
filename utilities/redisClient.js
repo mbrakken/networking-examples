b412c86bc0f50be409377f6cc9dc501a8edd6fe3
Marc Brakken <marc.brakken@gmail.com>
2022-04-23 11:31:48 -0500

    switch to heroku redis for v6

--- utilities/redisClient.js
+++ utilities/redisClient.js
@@ -1,6 +1,8 @@
 const { createClient } = require('redis');
 
-const redisURL = process.env.REDISTOGO_URL || "redis://localhost:6379"
+const redisURL = process.env.REDIS_TLS_URL || 'redis://localhost:6379';
 const client = createClient(redisURL);
+
+client.on('error', (err) => console.log('Redis Client Error', err));
 
 module.exports = client;
