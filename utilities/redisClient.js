f8001208c426781a5d9bda9898486378b3283f5b
Marc Brakken <marc.brakken@gmail.com>
2022-04-23 11:41:09 -0500

    trying to get heroku redis config in place

--- utilities/redisClient.js
+++ utilities/redisClient.js
@@ -1,6 +1,6 @@
 const { createClient } = require('redis');
 
-const redisURL = process.env.REDIS_TLS_URL || 'redis://localhost:6379';
+const redisURL = process.env.REDIS_TLS_URL ||  process.env.REDIS_URL || 'redis://localhost:6379';
 const client = createClient(redisURL);
 
 client.on('error', (err) => console.log('Redis Client Error', err));
