101cba3a0d390bd4145eb5d8d3d923da892b0a1e
Marc Brakken <marc.brakken@gmail.com>
2022-04-23 11:47:22 -0500

    why isn't this setting?

--- utilities/redisClient.js
+++ utilities/redisClient.js
@@ -1,8 +1,14 @@
 const { createClient } = require('redis');
 
-console.log("ENV", process.env);
+console.log('ENV', process.env);
 
-const redisURL = process.env.REDIS_TLS_URL ||  process.env.REDIS_URL || 'redis://localhost:6379';
+const redisURL =
+  process.env.REDIS_TLS_URL ||
+  process.env.REDIS_URL ||
+  process.env.REDISTOGO_URL;
+
+console.log('redisURL', redisURL);
+
 const client = createClient(redisURL);
 
 client.on('error', (err) => console.log('Redis Client Error', err));
